96c99f02106ffa171855b8829f42691e5cec5f8c
Wang Lu Lu <wang.lulu@vpclub.cn>
2018-09-28 20:33:00 +0800

    crd demo

--- examples/samplecontroller/main_test.go
+++ examples/samplecontroller/main_test.go
@@ -8,10 +8,13 @@ import (
 	"github.com/hidevopsio/hioak/starter/kube"
 	samplecontrollerv1alpha1 "github.com/hidevopsio/mioclient/examples/samplecontroller/pkg/apis/samplecontroller/v1alpha1"
 	clientset "github.com/hidevopsio/mioclient/examples/samplecontroller/pkg/client/clientset/versioned"
+	"github.com/stretchr/testify/assert"
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
-
 
 type sampleController struct {
 	restConfig *kube.RestConfig
@@ -23,46 +26,57 @@ func newSampleController(restConfig *kube.RestConfig) *sampleController {
 	}
 }
 
-func (c *sampleController) Register() error {
+const (
+	crdName string = "foos.samplecontroller.k8s.io"
+	fooName string = "example-foo"
+)
 
-	//clientSet, err := apiextensionsclient.NewForConfig(c.restConfig.Config)
-	//	//if err != nil {
-	//	//	log.Debugf("Error building example clientset: %s", err.Error())
-	//	//	return err
-	//	//}
-	//
-	//apiExt := clientSet.ApiextensionsV1beta1()
-	//crd := apiExt.CustomResourceDefinitions()
-	//list, err := crd.List(metav1.ListOptions{})
-	//if err != nil {
-	//	return err
-	//}
-	//log.Debugf("%v", list)
+func (c *sampleController) TestCreateCRD() error {
 
-	//
-	//crd := &apiextensionsv1beta1.CustomResourceDefinition{
-	//	ObjectMeta: metav1.ObjectMeta{Name: "foos.samplecontroller.k8s.io"},
-	//	Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-	//		Group:   "samplecontroller.k8s.io",
-	//		Version: "v1alpha1",
-	//		Scope:   apiextensionsv1beta1.NamespaceScoped,
-	//		Names:   apiextensionsv1beta1.CustomResourceDefinitionNames{
-	//			Plural: "foos",
-	//			Kind:   "Foo",
-	//		},
-	//	},
-	//}
-	//
-	//if _,err := clientSet.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd);err != nil {
-	//	log.Debugf("Error building example clientset: %s", err)
-	//	//return err
-	//}
+	clientSet, err := apiextensionsclient.NewForConfig(c.restConfig.Config)
+	if err != nil {
+		log.Debugf("Error building example clientset: %s", err.Error())
+		return err
+	}
+
+	crd := &apiextensionsv1beta1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{Name: crdName},
+		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
+			Group:   "samplecontroller.k8s.io",
+			Version: "v1alpha1",
+			Scope:   apiextensionsv1beta1.NamespaceScoped,
+			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+				Plural: "foos",
+				Kind:   "Foo",
+			},
+		},
+	}
+
+	if _, err := clientSet.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd); err != nil {
+		log.Debugf("Error building example clientset: %s", err)
+		//return err
+	}
 
 	return nil
 }
 
+func (c *sampleController) TestDeleteCRD() error {
 
-func (c *sampleController) CreateCrd()error{
+	clientSet, err := apiextensionsclient.NewForConfig(c.restConfig.Config)
+	if err != nil {
+		log.Debugf("Error building example clientset: %s", err.Error())
+		return err
+	}
+
+	if err := clientSet.ApiextensionsV1beta1().CustomResourceDefinitions().Delete(crdName, &metav1.DeleteOptions{}); err != nil {
+		log.Debugf("Error building example clientset: %s", err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *sampleController) TestFooClient() error {
 	//create foo crd
 	exampleClient, err := clientset.NewForConfig(c.restConfig.Config)
 	if err != nil {
@@ -70,27 +84,20 @@ func (c *sampleController) CreateCrd()error{
 		return err
 	}
 	foo := exampleClient.SamplecontrollerV1alpha1().Foos("default")
-	fooName := "example-foo"
 
 	// create foo
 	replica := int32(1)
 	f := &samplecontrollerv1alpha1.Foo{
 		ObjectMeta: metav1.ObjectMeta{Name: "example-foo"},
-		Spec:samplecontrollerv1alpha1.FooSpec{
-			DeploymentName:	fooName,
-			Replicas: &replica,
+		Spec: samplecontrollerv1alpha1.FooSpec{
+			DeploymentName: fooName,
+			Replicas:       &replica,
 		},
 	}
 
-	if _,err:= foo.Create(f);err != nil {
+	if _, err := foo.Create(f); err != nil {
 		log.Debugf("Error : %v", err)
 		//return err
-	}
-
-	// get one
-	if _,err := foo.Get("example-foo",metav1.GetOptions{});err != nil {
-		log.Debugf("Error : %v", err)
-		return err
 	}
 
 	// list
@@ -99,37 +106,42 @@ func (c *sampleController) CreateCrd()error{
 		log.Debugf("Error : %v", err)
 		return err
 	}
-
-
 	if len(fooList.Items) == 0 {
 		err := fmt.Errorf("list foo faile.")
 		log.Debugf("Error : %v", err)
 		return err
 	}
 
-	//update
-	replica = int32(2)
-	f.Spec.Replicas = &replica
-	if _,err := foo.Update(f);err != nil {
-		log.Debugf("Error : %v", err)
-		return err
-	}
-
-	//watch
-	watchFoo,err := foo.Watch(metav1.ListOptions{})
+	// get one
+	fooStruct, err := foo.Get("example-foo", metav1.GetOptions{})
 	if err != nil {
 		log.Debugf("Error : %v", err)
 		return err
 	}
 
-	if _,ok := <-watchFoo.ResultChan();!ok {
+	//update
+	replica = int32(2)
+	fooStruct.Spec.Replicas = &replica
+	if _, err := foo.Update(fooStruct); err != nil {
+		log.Debugf("Error : %v", err)
+		return err
+	}
+
+	//watch
+	watchFoo, err := foo.Watch(metav1.ListOptions{})
+	if err != nil {
+		log.Debugf("Error : %v", err)
+		return err
+	}
+
+	if _, ok := <-watchFoo.ResultChan(); !ok {
 		err := fmt.Errorf("foo watch faile")
 		log.Debugf("Error : %v", err)
 		return err
 	}
 
 	// delete
-	if err := foo.Delete(fooName,&metav1.DeleteOptions{});err != nil {
+	if err := foo.Delete(fooName, &metav1.DeleteOptions{}); err != nil {
 		log.Debugf("Error : %v", err)
 		return err
 	}
@@ -147,10 +159,18 @@ func TestCrd(t *testing.T) {
 	sampleController := testApp.GetInstance("sampleController").(*sampleController)
 
 	t.Run("|should register crd", func(t *testing.T) {
-		sampleController.Register()
+		err := sampleController.TestCreateCRD()
+		assert.Equal(t, nil, err)
 	})
 
 	t.Run("|should create crd", func(t *testing.T) {
-		sampleController.CreateCrd()
+		err := sampleController.TestFooClient()
+		assert.Equal(t, nil, err)
 	})
+
+	t.Run("|should delete crd foo", func(t *testing.T) {
+		err := sampleController.TestDeleteCRD()
+		assert.Equal(t, nil, err)
+	})
+
 }
