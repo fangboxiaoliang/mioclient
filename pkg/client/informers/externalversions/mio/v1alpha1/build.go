37cdf3ce17a31ad07651a77759ca4fb42a0b18c5
Wang Lu Lu <wang.lulu@vpclub.cn>
2018-10-18 14:50:16 +0800

    add field

--- pkg/client/informers/externalversions/mio/v1alpha1/build.go
+++ pkg/client/informers/externalversions/mio/v1alpha1/build.go
@@ -21,7 +21,7 @@ package v1alpha1
 import (
 	time "time"
 
-	mio_v1alpha1 "github.com/hidevopsio/mioclient/pkg/apis/mio/v1alpha1"
+	miov1alpha1 "github.com/hidevopsio/mioclient/pkg/apis/mio/v1alpha1"
 	versioned "github.com/hidevopsio/mioclient/pkg/client/clientset/versioned"
 	internalinterfaces "github.com/hidevopsio/mioclient/pkg/client/informers/externalversions/internalinterfaces"
 	v1alpha1 "github.com/hidevopsio/mioclient/pkg/client/listers/mio/v1alpha1"
@@ -70,7 +70,7 @@ func NewFilteredBuildInformer(client versioned.Interface, namespace string, resy
 				return client.MioV1alpha1().Builds(namespace).Watch(options)
 			},
 		},
-		&mio_v1alpha1.Build{},
+		&miov1alpha1.Build{},
 		resyncPeriod,
 		indexers,
 	)
@@ -81,7 +81,7 @@ func (f *buildInformer) defaultInformer(client versioned.Interface, resyncPeriod
 }
 
 func (f *buildInformer) Informer() cache.SharedIndexInformer {
-	return f.factory.InformerFor(&mio_v1alpha1.Build{}, f.defaultInformer)
+	return f.factory.InformerFor(&miov1alpha1.Build{}, f.defaultInformer)
 }
 
 func (f *buildInformer) Lister() v1alpha1.BuildLister {
